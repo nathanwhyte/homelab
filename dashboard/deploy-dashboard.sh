d501aa53606d7f023eb71f6dd902031696576c98
Nathan Whyte <nathanwhyte35@gmail.com>
2026-01-27 12:06:49 -0600

    Bring over dashboard deployment specs, add simple deploy script

---
+++ dashboard/deploy-dashboard.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+if [ ! -x "$(command -v "kubectl")" ]; then
+    echo "kubectl not installed installed."
+    exit 1
+fi
+
+if [ ! kubectl cluster-info > /dev/null 2>&1 ]; then
+     echo "kubectl not connected to a cluster."
+     exit 1
+fi
+
+if [ ! -x "$(command -v "helm")" ]; then
+     echo "helm not installed installed."
+     exit 1
+fi
+
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/ \
+
+echo "Updating helm repositories..."
+helm repo update > /dev/null
+
+helm upgrade --install kubernetes-dashboard \
+     kubernetes-dashboard/kubernetes-dashboard \
+    --create-namespace --namespace kubernetes-dashboard
+
+echo -e "\nApplying kubernetes-dashboard manifests..."
+
+if [ ! -f "ingress.yaml" ]; then
+    echo "ingress.yaml file not found!"
+    exit 1
+fi
+
+kubectl apply -f ingress.yaml
+
+if [ ! -f "user.yaml" ]; then
+     echo "user.yaml file not found!"
+     exit 1
+fi
+
+kubectl apply -f user.yaml
+
+if [ ! -f "cloudflared.secret.yaml" ]; then
+     echo "cloudflared.secret.yaml file not found!"
+     echo "Create this file and apply it, or manually create the cloudflare secret."
+else
+     kubectl apply -f cloudflared.secret.yaml
+fi
+
+if [ ! -f "cloudflared.yaml" ]; then
+     echo "cloudflared.yaml file not found!"
+     exit 1
+fi
+
+kubectl apply -f cloudflared.yaml
+
+echo -e "\nDone! Try these next:"
+echo "  kubectl get pods -n kubernetes-dashboard"
+echo "  kubectl logs -f -n kubernetes-dashboard deployments/kubernetes-dashboard-api"
+echo "  kubectl logs -f -n kubernetes-dashboard deployments/kubernetes-dashboard-kong"
